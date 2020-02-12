6c26014d45d984c0f303922a5294cc7b877df215
Mohit Muthanna Cheppudira <mohit@muthanna.com>
2020-02-11 19:51:37 -0500

    Added deployment script.

---
+++ deploy.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# This bucket is in the sweartax project
+DIST=./dist
+BUILD=$1
+
+BUCKET=gs://vexflow.com
+URL=${BUCKET}
+echo "Deploying to $BUCKET..."
+
+git diff-index --quiet HEAD --
+if [ "$?" != "0" ]
+then
+    echo Please commit changes before deploying.
+    exit 1
+fi
+
+echo Pushing local commits...
+git push
+if [ "$?" != "0" ]
+then
+    echo Could not push commits to Github.
+    exit 1
+fi
+
+echo Building bundles...
+npm run build
+if [ "$?" != "0" ]
+then
+    echo NPM build failed.
+    exit 1
+fi
+
+echo Uploading bundles...
+gsutil -h "Cache-control:public,max-age=86400" -m cp -a public-read -z js,map,svg \
+  $DIST/*.js  \
+  $DIST/*.map  \
+  $DIST/*.png  \
+  $DIST/*.svg  \
+  $DIST/favicon.ico  \
+  ${URL}
+
+gsutil -h "Cache-control:public,max-age=300" -m cp -a public-read -z html,css,js $DIST/index.html $DIST/style.css ${URL}
